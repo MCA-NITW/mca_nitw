632016a0080a6ac1a1c0ad5cc804083f8e9e31f9
Sachin Gupta <sachin.gupta.2j99@gmail.com>
2023-02-19 16:08:23 +0530

    middleware added

--- server/app.js
+++ server/app.js
@@ -11,6 +11,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use('/', (req, res, next) => {
+    res.send("<h1>Hello</h1>");
+})
+
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`)))
     .catch((error) => console.log(error.message));
