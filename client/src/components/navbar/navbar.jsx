028eeda8818afea3d56ea4d6ba4343dd64ce47d1
Sagar Gupta <sg85207@gmail.com>
2023-02-25 14:38:55 +0530

    Navbar Update
    Created New Navbar Self

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
-import './navbar.css'
+import classes from './navbar.module.css'
 
 const Nav = () => {
     return (
-        <nav>
-          <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
-            <li><a href="/log">Sign-in/Sign-up</a></li>
-          </ul>
+        <nav className={classes.main_nav}>
+          <div className={classes.nav_logo}>
+            <h1>Logo</h1>
+          </div>
+          <div className={classes.nav_links}>
+              <a href="/">Home</a>
+              <a href="/about">About us</a>
+              <a href="/contact">Contact</a>
+          </div>
+          <div className={classes.nav_btns}>
+            <button className={classes.btn}>Sign in/Sign up</button>
+          </div>
         </nav>
     )
 }
