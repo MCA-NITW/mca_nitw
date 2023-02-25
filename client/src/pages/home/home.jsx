881da292c3557866def206c0c1db26316ae726ed
Sagar Gupta <sg85207@gmail.com>
2023-02-25 19:21:58 +0530

    Logo + Home + Routers
    Logo Image + Home Screen + Routers
    
    Co-Authored-By: Sachin Gupta <sachin.gupta.2j99@gmail.com>

---
+++ client/src/pages/home/home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from "./home.module.css";
+
+const home = () => {
+  return (
+    <div>
+      <img
+        src="https://wallpapershome.com/images/pages/pic_h/23470.jpg"
+        alt="NITW"
+        className={classes.image}
+      />
+    </div>
+  );
+};
+
+export default home;
