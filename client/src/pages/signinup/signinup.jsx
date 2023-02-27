68a7d4c069189332009e678fee28c7069b093ba1
Sagar Gupta <sg85207@gmail.com>
2023-02-27 12:19:56 +0530

    Login-Signup
    Login-Signup

--- client/src/pages/signinup/signinup.jsx
+++ client/src/pages/signinup/signinup.jsx
@@ -1,27 +1,72 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import classes from './signinup.module.css'
 
 const signinup = () => {
   return (
-    <div className={classes.page} >
-      <div className={classes.page1}>
-        <h1>Login</h1>
-        <form>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
-        </form>
+    <div className={classes.main}>
+      <div className={classes.bckbtn}>
+        <Link to="/">
+          <button>Back</button>
+        </Link>
       </div>
-      <div>
-        <h1>Sign up</h1>
-        <form>
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm Password" />
-          <button type="submit">Sign up</button>
-        </form>
+      <div className={classes.signinup}>
+        <div className={classes.signin}>
+          <h1>Sign In</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Sign In</button>
+          </form>
+
+          <div className={classes.or}>
+            <hr />
+            <p>OR</p>
+            <hr />
+          </div>
+
+          <div className={classes.google}>
+            <button>Sign In with Google</button>
+          </div>
+
+          <div className={classes.forgot}>
+            <Link to="/forgotpassword">
+              <p>Forgot Password?</p>
+            </Link>
+            <Link to="/signup">
+              <p>Sign Up</p>
+            </Link>
+          </div>
+        </div>
+        <div className={classes.signup}>
+          <h1>Sign Up</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <input type="password" placeholder="Confirm Password" />
+            <button>Sign Up</button>
+          </form>
+
+          <div className={classes.or}>
+            <hr />
+            <p>OR</p>
+            <hr />
+          </div>
+
+          <div className={classes.google}>
+            <button>Sign Up with Google</button>
+          </div>
+          <div className={classes.forgot}>
+            <Link to="/forgotpassword">
+              <p>Forgot Password?</p>
+            </Link>
+
+            <Link to="/signin">
+              <p>Sign In</p>
+            </Link>
+          </div>
+        </div>
       </div>
-      
     </div>
   );
 }
