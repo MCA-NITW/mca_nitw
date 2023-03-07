d20043636d610c5cbd3f7597d70b01d083181506
Sagar Gupta <sg85207@gmail.com>
2023-03-07 14:11:53 +0530

    Profile Page Structure
    Structure Done CSS Remaining

--- client/src/Layout/AuthNavigation.jsx
+++ client/src/Layout/AuthNavigation.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import classes from "./AuthNavigation.module.css";
-import FastRewindIcon from '@mui/icons-material/FastRewind';
+import {MdFastRewind} from 'react-icons/md';
 
 const AuthNavigation = () => {
   return (
     <div className={classes.main}>
       <Link to="/">
-        <FastRewindIcon sx={{ fontSize: "25px" }} />
+        <MdFastRewind/>
         <span> Back </span>
       </Link>
+      <div className={classes.logo}>
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/013/389/534/non_2x/mca-letter-logo-design-in-illustration-logo-calligraphy-designs-for-logo-poster-invitation-etc-free-vector.jpg"
+          alt="Logo"
+        />
+      </div>
     </div>
   );
 };
