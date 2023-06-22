fdb233043e9d9ce5a59799f3b6ae053e7ef25097
Jigyashu <jigyashusingh01@gmail.com>
2023-06-22 23:45:38 +0530

    Added Footer Component

---
+++ client/src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './footer.module.css'
+
+const footer = () => {
+  return (
+    <div className={classes.main}>
+        <p>Made by @MCA_Students</p>
+    </div>
+  )
+}
+
+export default footer
